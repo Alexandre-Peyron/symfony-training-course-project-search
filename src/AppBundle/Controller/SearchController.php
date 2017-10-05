1d45a9d7c00ed71dcff3e36651ad8a74abf733c4
Alexandre Peyron <alpeyron@gmail.com>
2017-10-05 14:55:52 +0200

    add first controller, view and bootstrap

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -6,16 +6,16 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class SearchController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="search")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('search/index.html.twig', [
+
         ]);
     }
 }
