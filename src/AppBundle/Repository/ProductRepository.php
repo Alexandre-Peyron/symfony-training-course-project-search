b82319c4b67fb5246ae918c2f2313931a6f65d3c
Alexandre Peyron <alpeyron@gmail.com>
2017-10-09 23:53:40 +0200

    fix query filter

--- src/AppBundle/Repository/ProductRepository.php
+++ src/AppBundle/Repository/ProductRepository.php
@@ -23,7 +23,7 @@ class ProductRepository extends \Doctrine\ORM\EntityRepository
     {
         $queryBuilder = $this->queryFilterByAllCriteria($criteria);
 
-        if(is_array($pager)) {
+        if (is_array($pager)) {
             $queryBuilder
                 ->setFirstResult($pager['currentPosition'])
                 ->setMaxResults($pager['nbResultByPage']);
@@ -62,47 +62,46 @@ class ProductRepository extends \Doctrine\ORM\EntityRepository
 
         $queryBuilder->where('p.isEnabled = 1');
 
-        if (isset($criteria[Criteria::SEARCH]) && !empty($criteria[Criteria::SEARCH])) {
+        if (!empty($criteria[Criteria::SEARCH])) {
             $queryBuilder->andWhere(
                 $queryBuilder->expr()->orX(
-                    $queryBuilder->expr()->like('p.name', ':search'),
-                    $queryBuilder->expr()->like('p.description', ':search'),
-                    $queryBuilder->expr()->like('p.reference', ':search')
-                ))
-                ->setParameter('search', '%' . $criteria[Criteria::SEARCH] . '%');
+                    $queryBuilder->expr()->like('p.name', $queryBuilder->expr()->literal('%' . $criteria[Criteria::SEARCH] . '%')),
+                    $queryBuilder->expr()->like('p.description', $queryBuilder->expr()->literal('%' . $criteria[Criteria::SEARCH] . '%')),
+                    $queryBuilder->expr()->like('p.reference', $queryBuilder->expr()->literal('%' . $criteria[Criteria::SEARCH] . '%'))
+                ));
         }
 
-        if (isset($criteria[Criteria::PRICE_MIN]) && !empty($criteria[Criteria::PRICE_MIN])) {
+        if (!empty($criteria[Criteria::PRICE_MIN])) {
             $queryBuilder
                 ->andWhere('p.price >= :priceMin')
                 ->setParameter('priceMin', $criteria[Criteria::PRICE_MIN]);
         }
 
-        if (isset($criteria[Criteria::PRICE_MAX]) && !empty($criteria[Criteria::PRICE_MAX])) {
+        if (!empty($criteria[Criteria::PRICE_MAX])) {
             $queryBuilder
                 ->andWhere('p.price <= :priceMax')
                 ->setParameter('priceMax', $criteria[Criteria::PRICE_MAX]);
         }
 
-        if (isset($criteria[Criteria::NOTE])) {
+        if (!empty($criteria[Criteria::NOTE])) {
             $queryBuilder
                 ->andWhere('p.note >= :note')
                 ->setParameter('note', $criteria[Criteria::NOTE]);
         }
 
-        if (isset($criteria[Criteria::PREMIUM])) {
+        if (!empty($criteria[Criteria::PREMIUM])) {
             $queryBuilder->andWhere('p.isPremium = :isPremium')
                 ->setParameter('isPremium', $criteria[Criteria::PREMIUM]);
         }
 
-        if (isset($criteria[Criteria::TYPE]) && !empty($criteria[Criteria::TYPE])) {
+        if (!empty($criteria[Criteria::TYPE])) {
             $queryBuilder
                 ->leftJoin('p.type', 't')
                 ->andWhere('t.id = :typeId')
                 ->setParameter('typeId', $criteria[Criteria::TYPE]);
         }
 
-        if (isset($criteria[Criteria::BRAND]) && !empty($criteria[Criteria::BRAND])) {
+        if (!empty($criteria[Criteria::BRAND])) {
             $queryBuilder
                 ->leftJoin('p.brand', 'b')
                 ->andWhere('b.id = :brandId')
