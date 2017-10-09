85f5ab7857b2b73cd38e5ad6f58bf53d0d42830c
Alexandre Peyron <alpeyron@gmail.com>
2017-10-09 16:00:35 +0200

    display product in views

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
 use AppBundle\Form\ProductSearchType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -14,15 +15,19 @@ class SearchController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $formSearch = $this->createForm(ProductSearchType::class);
+        $em = $this->getDoctrine()->getManager();
 
+        $formSearch = $this->createForm(ProductSearchType::class);
         $formSearch->handleRequest($request);
 
         if ($formSearch->isSubmitted() && $formSearch->isValid()) {
             //TODO
         }
 
+        $products = $em->getRepository(Product::class)->findAll();
+
         return $this->render('search/index.html.twig', [
+            'products'    => $products,
             'form_search' => $formSearch->createView()
         ]);
     }
